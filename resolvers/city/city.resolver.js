8ab8e0706faa59c3c5e496a2d6f31030ce3e2233
Ameron666 <aisakebi69@gmail.com>
2025-01-18 14:52:55 +0300

    hf;

--- resolvers/city/city.resolver.js
+++ resolvers/city/city.resolver.js
@@ -3,8 +3,8 @@ import { prisma } from "../../prisma.js"
 const cityResolver = {
   Query: {
     citys: async () => {
-      return prisma.city.findMany({orderBy: { name: "asc" }})
-    },
+      return prisma.city.findMany({orderBy: { city: "asc" }})
+    },  
     city: async (_, { city }) => {
       return prisma.city.findMany({
         where: { city: { contains: city, mode: "insensitive" } }
