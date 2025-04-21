37efbb34306a832fccafd51d55f69359fd727175
Ameron666 <aisakebi69@gmail.com>
2025-04-21 14:24:34 +0300

    hf;

--- resolvers/hotel/hotel.resolver.js
+++ resolvers/hotel/hotel.resolver.js
@@ -1088,7 +1088,7 @@ const ensureNoOverlap = async (roomId, place, start, end, excludeId) => {
   })
 
   if (overlap) {
-    return new Error(
+    throw new Error(
       `Невозможно разместить заявку: пересечение с заявкой №${overlap.id} ` +
         `в комнате ${roomId}, месте ${place} ` +
         `(${overlap.start.toISOString()} – ${overlap.end.toISOString()})`
