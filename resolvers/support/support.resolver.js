724d7a08b73459911ea0fe05d5a079942d13765e
Ameron666 <aisakebi69@gmail.com>
2025-08-26 18:17:14 +0300

    disgusting fix;

--- resolvers/support/support.resolver.js
+++ resolvers/support/support.resolver.js
@@ -27,8 +27,8 @@ const supportResolver = {
     getAllDocumentations: async (_, __, context) => {
       await allMiddleware(context)
       return await prisma.documentation.findMany({
-        where: { parentId: null }, // ← только корневые
-        orderBy: { name: "asc" }, // можно "asc", если нужен алфавит
+        where: { parentId: { not: { not: null } } }, // ← только корневые
+        orderBy: { name: "asc" } // можно "asc", если нужен алфавит
       })
     },
 
