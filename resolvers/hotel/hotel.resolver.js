8d4178d31ed2e7a506dca066444dd6b0eccf8460
Ameron666 <aisakebi69@gmail.com>
2025-09-01 12:17:10 +0300

    disable dev foo;

--- resolvers/hotel/hotel.resolver.js
+++ resolvers/hotel/hotel.resolver.js
@@ -96,9 +96,8 @@ const hotelResolver = {
       })
 
       // await prisma.hotel.updateMany({ data: { show: false, meal: false } })
-
-      await prisma.hotel.updateMany({ data: { show: true, meal: true } })
-
+      // await prisma.hotel.updateMany({ data: { show: true, meal: true } })
+      
       const totalPages = take && !all ? Math.ceil(totalCount / take) : 1
 
       return { hotels, totalCount, totalPages }
