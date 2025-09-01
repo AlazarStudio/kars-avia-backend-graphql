72e4b7d64ddb300c91984316612de5bc0d737dad
Ameron666 <aisakebi69@gmail.com>
2025-09-01 11:48:42 +0300

    hf;

--- resolvers/hotel/hotel.resolver.js
+++ resolvers/hotel/hotel.resolver.js
@@ -94,6 +94,8 @@ const hotelResolver = {
         },
         orderBy: { information: { city: "asc" } }
       })
+      await prisma.hotel.updateMany({ data: { meal: false } })
+      // await prisma.hotel.updateMany({ data: { meal: true } })
 
       const totalPages = take && !all ? Math.ceil(totalCount / take) : 1
 
