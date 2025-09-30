0c00f826d8e5f4274b12b33139e8a1e188d837ee
Ameron666 <aisakebi69@gmail.com>
2025-10-01 00:02:52 +0300

    !hf sup chat;

--- resolvers/support/support.resolver.js
+++ resolvers/support/support.resolver.js
@@ -64,40 +64,51 @@ const supportResolver = {
         throw new Error("Access denied")
       }
 
-      // Возвращаем все чаты, в которых участвует пользователь (через таблицу ChatUser) и которые помечены как support (isSupport: true)
-      return await prisma.chat.findMany({
-        where: {
-          isSupport: true,
-          participants: {
-            some: {
-              userId: user.id // Проверяем, есть ли пользователь среди участников чата
-            }
-          }
-        },
-        include: {
-          participants: {
-            include: {
-              user: {
-                select: {
-                  id: true,
-                  name: true,
-                  number: true,
-                  images: true,
-                  role: true,
-                  position: true,
-                  airlineId: true,
-                  airlineDepartmentId: true,
-                  hotelId: true,
-                  dispatcher: true
-                }
+      // Возвращаем все чаты, в которых участвует пользователь, не являющийся саппортом
+      return await prisma.chat
+        .findMany({
+          where: {
+            isSupport: true, // Только чаты, помеченные как поддержка
+            participants: {
+              some: {
+                userId: user.id // Проверяем, есть ли пользователь среди участников чата
               }
             }
           },
-          messages: true
-        }
-      })
+          include: {
+            participants: {
+              include: {
+                user: {
+                  select: {
+                    id: true,
+                    name: true,
+                    number: true,
+                    images: true,
+                    role: true,
+                    position: true,
+                    airlineId: true,
+                    airlineDepartmentId: true,
+                    hotelId: true,
+                    dispatcher: true
+                  }
+                }
+              }
+            },
+            messages: true
+          }
+        })
+        .then((chats) => {
+          // Фильтруем чаты, чтобы получить только пользователей, которые не являются саппортом
+          return chats
+            .map((chat) => {
+              const nonSupportUser = chat.participants.find(
+                (participant) => !participant.user.support
+              )
+              return nonSupportUser ? nonSupportUser.user : null
+            })
+            .filter((user) => user !== null)
+        })
     },
-
     // Query: userSupportChat
     // Возвращает чат поддержки, связанный с указанным userId.
     // Если чат не найден, создается новый чат поддержки, в который добавляются указанный пользователь и все агенты поддержки.
